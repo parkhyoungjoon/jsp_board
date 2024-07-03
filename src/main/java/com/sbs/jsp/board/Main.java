164acecf57fd987df1b7d6a112a9468ed95155fb
parkhyoungjoon <hyongjoon123@naver.com>
2024-07-03 17:28:58 +0900

    서블릿 라이브러리 설치, HelloServlet 추가

--- src/main/java/com/sbs/jsp/board/Main.java
+++ src/main/java/com/sbs/jsp/board/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.sbs.jsp.board.example;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/main/java/com/sbs/jsp/board/servlet/HelloServlet.java
@@ -0,0 +1,2 @@
+package com.sbs.jsp.board.servlet;public class HelloServlet {
+}
